0384225e5d2dd9e46f97b9ee634f1a9ddf3aa497
LILouis <lijunpeng91@gmail.com>
2018-09-28 19:12:34 +0800

    grap perlin noise function from processing.js

--- PixiJS/assets/js/test_1.js
+++ PixiJS/assets/js/test_1.js
@@ -1,62 +1,87 @@
+let mode = 0;
+
 const winWidth = 1000;
 const winHeight = 1000;
+let movingY = 0;
+let movingStep = 5;
+let framePerStep = 60;
+let currentFrameCount = 0;
 
 var renderer = PIXI.autoDetectRenderer(winWidth, winHeight, {backgroundColor : 0x000000});
 document.body.appendChild(renderer.view);
 
 // 创建舞台 Container
 // 之后的对象都存在于舞台之上
-var stage = new PIXI.Container();
+stage = new PIXI.Container();
 
 
 PIXI.loader
-  .add("point", "/assets/img/point.png")
-  .load(setup);
+	.add("point", "/assets/img/point.png")
+  	.load(setup);
 
 function setup() {
   //This code will run when the loader has finished loading the image
 
+  	if (mode == 0) {
 
-	let row = 100;
-	let col = 100;
-	let width = parseInt(winWidth / row);
-	let count = 0;
-let test = [];
+		let row = 100;
+		let col = 100;
+		let width = parseInt(winWidth / row);
+		let count = 0;
 
-	for (let r = 0; r < row; r++) {
-	    for (let c = 0; c < col; c++) {
-	        let noise = Math.abs(PerlinNoise(r, c) / 2);
-
-			// let texture = PIXI.utils.TextureCache["point"];
-	  //       let sprite = new PIXI.Sprite(texture);
-	  //       sprite.x = c * width;
-	  //       sprite.y = r * width;
-	  //       sprite.alpha = noise;
-
-	  //       stage.addChild(sprite);
-	        // console.log(noise);
-
-	        noise = parseInt(noise * 255);
-	        noise = noise.toString(16);
-	        noise = noise.length == 1 ? "0" + noise : noise;
-			let color = "0xAAFF" + noise.toString(16);
-			// console.log(color);
-
-			let rectangle = new PIXI.Graphics();
-			rectangle.beginFill(parseInt(color, 16));
-			// rectangle.beginFill(0xFFFFFF, noise);
-			rectangle.drawRect(0, 0, width, width);
-			rectangle.endFill();
-			rectangle.x = c * width;
-			rectangle.y = r * width;
-			stage.addChild(rectangle);
-
-			test.push(noise);
-			count++;
+		let noiseArray = [];
+		let maxNoise = -1000000;
+		let minNoise = 1000000;
+		for (let r = 0; r < row; r++) {
+			noiseArray[r] = [];
+		    for (let c = 0; c < col; c++) {
+		    	let tempNoise = PerlinNoise(r * 0.1, c * 0.1);
+		    	noiseArray[r][c] = tempNoise;
+		    	maxNoise = maxNoise > tempNoise ? maxNoise : tempNoise;
+		    	minNoise = minNoise < tempNoise ? minNoise : tempNoise;
+	    	}
 	    }
-	}
+		
+	let test = [];
+	    let noiseScale = maxNoise - minNoise;
+		for (let r = 0; r < row; r++) {
+		    for (let c = 0; c < col; c++) {
+		        // let noise = p5PerlinNoise(r * 0.1, c * 0.1);
+		        let noise = (noiseArray[r][c] - minNoise) / noiseScale;
 
-	console.log(count);
+				// let texture = PIXI.utils.TextureCache["point"];
+		  //       let sprite = new PIXI.Sprite(texture);
+		  //       sprite.x = c * width;
+		  //       sprite.y = r * width;
+		  //       sprite.alpha = noise;
+
+		  //       stage.addChild(sprite);
+		        // console.log(noise);
+
+		        // noise = parseInt(noise * 255);
+		        // noise = noise.toString(16);
+		        // noise = noise.length == 1 ? "0" + noise : noise;
+				// let color = "0xAAFF" + noise.toString(16);
+				// console.log(color);
+
+				let rectangle = new PIXI.Graphics();
+				// rectangle.beginFill(parseInt(color, 16));
+				rectangle.beginFill(0xFFFFFF, noise);
+				rectangle.drawRect(0, 0, width, width);
+				rectangle.endFill();
+				rectangle.x = c * width;
+				rectangle.y = r * width;
+				stage.addChild(rectangle);
+
+				test.push(noise);
+				// count++;
+		    }
+		}
+
+		console.log(test);
+	} else if (mode == 1) {
+		updateNoise();
+	}
 
 }
 
@@ -73,12 +98,48 @@ function animate() {
     requestAnimationFrame(animate);
 
     // 渲染 Container
-    renderer.render(stage);
+    if (mode == 0) {
+    	renderer.render(stage);
+    } else if (mode == 1) {
+    	currentFrameCount++;
+    	if (currentFrameCount >= framePerStep) {
+    		currentFrameCount = 0;
+    		movingY += movingStep;
+			updateNoise();
+    	} 
+    }
+}
+
+function updateNoise() {
+	stage.removeChildren();
+
+	console.log("update");
+
+	let row = 100;
+	let col = 100;
+	let width = parseInt(winWidth / row);
+
+	for (let r = 0; r < row; r++) {
+	    for (let c = 0; c < col; c++) {
+	    	let tempR = r + movingY;
+	        let noise = p5PerlinNoise(tempR * 0.1, c * 0.1);
+	        // let noise = (noiseArray[r][c] - minNoise) / noiseScale;
+
+			let rectangle = new PIXI.Graphics();
+			// rectangle.beginFill(parseInt(color, 16));
+			rectangle.beginFill(0xFFFFFF, noise);
+			rectangle.drawRect(0, 0, width, width);
+			rectangle.endFill();
+			rectangle.x = c * width;
+			rectangle.y = r * width;
+			stage.addChild(rectangle);
+	    }
+	}
 }
 
 function PerlinNoise (x, y) {
-    let persistence = 1 / 4;
-    let Number_Of_Octaves = 4;
+    let persistence = 1 / 2;
+    let Number_Of_Octaves = 8;
 
     let noise = 0;
     let p = persistence;
@@ -142,6 +203,100 @@ let InterpolatedNoise = function (x, y) {  // 获取插值噪声
     return Cosine_Interpolate(i1, i2, fractional_Y);
 }
 
+// ===============================
+var PERLIN_YWRAPB = 4;
+var PERLIN_YWRAP = 1 << PERLIN_YWRAPB;
+var PERLIN_ZWRAPB = 8;
+var PERLIN_ZWRAP = 1 << PERLIN_ZWRAPB;
+var PERLIN_SIZE = 4095;
 
+var perlin_octaves = 4; // default to medium smooth
+var perlin_amp_falloff = 0.5; // 50% reduction/octave
+
+var scaled_cosine = function(i) {
+  return 0.5 * (1.0 - Math.cos(i * Math.PI));
+};
+
+var perlin; 
+
+let p5PerlinNoise = function(x, y, z) {
+  	y = y || 0;
+  	z = z || 0;
+
+  	if (perlin == null) {
+    	perlin = new Array(PERLIN_SIZE + 1);
+    	for (var i = 0; i < PERLIN_SIZE + 1; i++) {
+      		perlin[i] = Math.random();
+    	}
+  	}
+
+  	if (x < 0) {
+    	x = -x;
+  	}
+  	if (y < 0) {
+    	y = -y;
+  	}
+  	if (z < 0) {
+    	z = -z;
+  	}
+
+  	var xi = Math.floor(x),
+    yi = Math.floor(y),
+    zi = Math.floor(z);
+  	var xf = x - xi;
+  	var yf = y - yi;
+  	var zf = z - zi;
+  	var rxf, ryf;
+
+  	var r = 0;
+  	var ampl = 0.5;
+
+	var n1, n2, n3;
+
+  	for (var o = 0; o < perlin_octaves; o++) {
+   		var of = xi + (yi << PERLIN_YWRAPB) + (zi << PERLIN_ZWRAPB);
+
+    	rxf = scaled_cosine(xf);
+    	ryf = scaled_cosine(yf);
+
+   		n1 = perlin[of & PERLIN_SIZE];
+    	n1 += rxf * (perlin[(of + 1) & PERLIN_SIZE] - n1);
+    	n2 = perlin[(of + PERLIN_YWRAP) & PERLIN_SIZE];
+    	n2 += rxf * (perlin[(of + PERLIN_YWRAP + 1) & PERLIN_SIZE] - n2);
+    	n1 += ryf * (n2 - n1);
+
+    	of += PERLIN_ZWRAP;
+    	n2 = perlin[of & PERLIN_SIZE];
+    	n2 += rxf * (perlin[(of + 1) & PERLIN_SIZE] - n2);
+    	n3 = perlin[(of + PERLIN_YWRAP) & PERLIN_SIZE];
+    	n3 += rxf * (perlin[(of + PERLIN_YWRAP + 1) & PERLIN_SIZE] - n3);
+    	n2 += ryf * (n3 - n2);
+
+    	n1 += scaled_cosine(zf) * (n2 - n1);
+
+    	r += n1 * ampl;
+    	ampl *= perlin_amp_falloff;
+    	xi <<= 1;
+    	xf *= 2;
+    	yi <<= 1;
+    	yf *= 2;
+    	zi <<= 1;
+    	zf *= 2;
+
+    	if (xf >= 1.0) {
+			xi++;
+			xf--;
+    	}
+    	if (yf >= 1.0) {
+      		yi++;
+     	 	yf--;
+    	}
+    	if (zf >= 1.0) {
+    		  zi++;
+     		 zf--;
+    	}
+  	}
+	return r;
+};
 
 
